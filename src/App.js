1790b1a167e46f9adce7e087a3ca2f87a6eda9f9
repro-tetris <77537560+repro-tetris@users.noreply.github.com>
2021-08-31 23:14:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div></div>;
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return <div></div>;
+  }
 }
 
 export default App;
